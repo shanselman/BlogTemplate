843c485bed1239a83dedff8a96d1f42fa0e8f522
t-luisaz <lisaza@stanford.edu>
2017-07-18 13:13:40 -0700

    Creating User Registration Page

---
+++ BlogTemplate/Models/Owner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogTemplate.Models
+{
+    public class Owner
+    {
+        public string LoginEmail { get; set; }
+        public string LoginName { get; set; }
+        public string LoginPassword { get; set; }
+    }
+}
