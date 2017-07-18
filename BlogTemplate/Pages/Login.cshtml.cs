98588823db03beb07b6be1f11ecb03a7383be5d8
t-luisaz <lisaza@stanford.edu>
2017-07-18 11:39:27 -0700

    Validation for email and password

--- BlogTemplate/Pages/Login.cshtml.cs
+++ BlogTemplate/Pages/Login.cshtml.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BlogTemplate.Models;
 
 namespace BlogTemplate.Pages
 {
     public class LoginModel : PageModel
     {
-        public string NewMessage { get; set; }
+        public Login Login { get; set; }
         public void OnGet()
         {
-            NewMessage = "here is what you want to put on the page.";
+            
         }
     }
 }
